86eeb25da486cdfc0a489bdfc2a8050663826e18
严尧琦 <yyq@winning.com.cn>
2016-10-24 13:58:37 +0800

    为webapp加入测试代码,测试埋点数据

---
+++ monitor-website/src/test/java/com/winning/monitor/website/test/LoggingUT.java
@@ -0,0 +1,34 @@
+package com.winning.monitor.website.test;
+
+import com.winning.monitor.agent.logging.MonitorLogger;
+import com.winning.monitor.agent.logging.transaction.Transaction;
+import org.junit.Test;
+
+/**
+ * Created by nicholasyan on 16/10/24.
+ */
+public class LoggingUT {
+
+    @Test
+    public void testTransactionData() throws InterruptedException {
+        MonitorLogger.checkAndInitialize();
+
+        while (true) {
+            MonitorLogger.setCaller("HIS", "192.16.0.1", "PC");
+            Transaction parentTransaction = MonitorLogger.beginTransactionType("挂号");
+            parentTransaction.addData("data1", "data1");
+
+            Transaction childTransaction = MonitorLogger.beginTransactionName(parentTransaction, "读取数据库");
+            Thread.sleep(10);
+            childTransaction.success();
+
+            childTransaction = MonitorLogger.beginTransactionName(parentTransaction, "写入数据库");
+            Thread.sleep(10);
+            childTransaction.success();
+
+            parentTransaction.success();
+            Thread.sleep(1000);
+        }
+    }
+
+}
