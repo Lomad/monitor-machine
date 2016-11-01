4f4fecf6d1a0e6fc34361c8c790df4fc29ce31db
Evan <myj@winning.com.cn>
2016-11-01 17:34:24 +0800

    历史服务方接口增加

--- monitor-website/src/main/webapp/js/clienthistory.js
+++ monitor-website/src/main/webapp/js/clienthistory.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         global_Object.queryTableData();
         var html ='';
         $.each(data,function(i,v){
-         html+='<li class="cp" role="presentation"><a role="menuitem" tabindex="-1">'+v+'</a></li>'
+            html+='<li class="cp" role="presentation"><a role="menuitem" tabindex="-1">'+v+'</a></li>'
         });
         $("#fl").html(html);
         $("#fl a").on("click",function(){
@@ -23,13 +23,10 @@ $(document).ready(function(){
 });
 
 var global_Object={
-    tableDataOld: [],
     tableData:[],
     totalSize: 0,
     flname:"",
     url: contextPath+"/paas/queryTodayTransactionTypeReportByServer",
-    dttype:"",
-    dttime:"",
     initDomEvent:function(){
         $("#date_picker").datepicker({
             language: "zh-CN",
@@ -41,66 +38,21 @@ var global_Object={
         var date = new Date();
         var yesterday = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate()-1);
         $("#date_picker").datepicker('update',yesterday);
-        // 日历类型选择器修改时间的触发锚点: 下传时间类型和时间字符串，并post到不同的URL
         $("#sel a").on('click',function(){
             var data =$(this).attr("data");
             $("#selbtn").html($(this).text()+' <i class="fa  fa-caret-down"></i>');
             global_Object.selectType(data);
-
-            global_Object.dttype = data;
-            global_Object.dttime = $('#datevalue').val();
-            if(global_Object.dttype == "day"){
-                global_Object.url = contextPath + "/paas/queryDayTransactionTypeReportByClient";
-                global_Object.queryTableData();
-            }else if(global_Object.dttype == "week"){
-                global_Object.url = contextPath + "/paas/queryWeekTransactionTypeReportByClient";
-                global_Object.queryTableData();
-            }else if(global_Object.dttype == "month"){
-                global_Object.url = contextPath + "/paas/queryMonthTransactionTypeReportByClient";
-                global_Object.queryTableData();
-            }
-            //alert( global_Object.dttime );
         });
-        // 日历选择器修改时间的触发锚点
         $("#date_picker").datepicker().on('hide', function(){
             var date = $('#datevalue').val();
-            if(global_Object.type=="week" && date!=""){
+            if(global_Object.type=="week"&&date!=""){
                 var newdate = global_Object.getNewDay(date);
                 $('#datevalue').val(newdate);
+            }else if(global_Object.viewId==2&&date==""){
+                var date = new Date();
+                var week = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
+                $("#datevalue").val(global_Object.getNewDay(week));
             }
-            //alert($('#datevalue').val());
-            global_Object.dttime = $('#datevalue').val();
-            if(global_Object.dttype == "day"){
-                global_Object.url = contextPath + "/paas/queryDayTransactionTypeReportByClient";
-                global_Object.queryTableData();
-            }else if(global_Object.dttype == "week"){
-                global_Object.url = contextPath + "/paas/queryWeekTransactionTypeReportByClient";
-                global_Object.queryTableData();
-            }else if(global_Object.dttype == "month"){
-                global_Object.url = contextPath + "/paas/queryMonthTransactionTypeReportByClient";
-                global_Object.queryTableData();
-            }
-            //alert( global_Object.dttime );
-        });
-        //$("#date_picker").datepicker().on('changeDate', function(){
-        //    alert("date-picker:"+$('#datevalue').val());
-        //});
-
-        /* 查询框的过滤的功能 */
-        $("#querybtn").on("click",function(){
-            global_Object.setTableData("search",this);
-        });
-        /* 可排序字段的排序功能 */
-        $("#fTable .sorting").on("click", function () {
-            if ($(this).hasClass("desc")) {
-                $(this).addClass("asc").removeClass("desc");
-                global_Object.setTableData("asc",this);
-            }
-            else {
-                $(this).addClass("desc").removeClass("asc");
-                global_Object.setTableData("desc",this);
-            }
-
         });
 
     },
@@ -173,65 +125,12 @@ var global_Object={
         return (getdate(monday)+"-"+getdate(millSeconds));
     },
     queryTableData:function(){
-        $.post(global_Object.url,{flname:global_Object.flname,dttype:global_Object.dttype, dttime:global_Object.dttime},function(data){
-            global_Object.tableDataOld =data.transactionStatisticDatas;
+        $.post(global_Object.url,{flname:global_Object.flname},function(data){
+            //console.log(data);
             global_Object.totalSize = data.totalSize;
             global_Object.tableData = data.transactionStatisticDatas;
             global_Object.setTable();
         });
-    },
-    setTableData:function(type,obj){
-        if(type =="search"){
-            var tableData=[];
-            $.each(global_Object.tableDataOld,function(i,v){
-                if(v.transactionTypeName.indexOf($.trim($("#keyword").val()))>-1){
-                    tableData.push(v);
-                }
-            });
-            global_Object.tableData=tableData;
-            //console.log(global_Object.tableData);
-        }
-        else if(type=="asc"){
-            var id= $(obj).data("id");
-            //冒泡排序
-            var array = global_Object.tableData;
-            var temp =0;
-            for (var i = 0; i < array.length; i++)
-            {
-                for (var j = 0; j < array.length - i-1; j++)
-                {
-                    if (array[j][id] > array[j + 1][id])
-                    {
-                        temp = array[j + 1];
-                        array[j + 1] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-            global_Object.tableData=array;
-            //console.log(global_Object.tableData);
-        }
-        else if(type=="desc"){
-            var id= $(obj).data("id");
-            var array =global_Object.tableData;
-            //var array = [3,5,1,6];
-            var temp =0;
-            for (var i = 0; i < array.length; i++)
-            {
-                for (var j = 0; j < array.length - i-1; j++)
-                {
-                    if (array[j][id] < array[j + 1][id])
-                    {
-                        temp = array[j + 1];
-                        array[j + 1] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-            global_Object.tableData=array;
-            //console.log(global_Object.tableData);
-        }
-        global_Object.setTable();
     },
     setTable:function(){
         var alltr = function(length,i,data){
@@ -247,8 +146,7 @@ var global_Object={
             tr += '<td>' + data.min + 'ms</td>';
             tr += '<td>' + data.max + 'ms</td>';
             tr += '<td>' + data.tps + '</td>';
-            //tr += '<td>' + data.failCount + '次</td>';
-            tr += '<td><a onclick="global_Object.openPostFalse(this)" href="javascript:void(0)">' + data.failCount + '次</a></td>';
+            tr += '<td>' + data.failCount + '次</td>';
             tr += '<td>' + data.failPercent*100 + '%</td>';
             tr += '<td>' + data.std + 'ms</td>';
             tr += '<td><i class="fa  fa-bar-chart-o cp" onclick="global_Object.queryPic(this)"></i></td>';
@@ -321,23 +219,18 @@ var global_Object={
     },
     openPostAvg:function(obj){
         global_Object.value = $("#datevalue").val();
-        var url =contextPath+"/paas/clientstephistory";
+        var url =contextPath+"/paas/serverstephistory";
         //alert($(obj).parents("tr").data("transactiontypename"))
         var datas={"transactionTypeName":$(obj).parents("tr").data("transactiontypename"),"serverIpAddress":$(obj).parents("tr").data("serveripaddress")==undefined?"":$(obj).parents("tr").data("serveripaddress"),"serverAppName":global_Object.flname,"type":(global_Object.type==undefined?"":global_Object.type),value:global_Object.value,historyPageType:"client"};
         JqCommon.openPostWindow(url,datas);
     },
     openPostTotalCount:function(obj){
         global_Object.value = $("#datevalue").val();
-        var url =contextPath+"/paas/clientdetailedhistory";
+        var url =contextPath+"/paas/serverdetailedhistory";
         //alert($(obj).parents("tr").data("transactiontypename"))
         var datas={"transactionTypeName":$(obj).parents("tr").data("transactiontypename"),"serverIpAddress":$(obj).parents("tr").data("serveripaddress")==undefined?"":$(obj).parents("tr").data("serveripaddress"),"serverAppName":global_Object.flname,"type":(global_Object.type==undefined?"":global_Object.type),value:global_Object.value,"clientAppName":"","clientIpAddress":"","status":"",historyPageType:"client"};
         console.log(datas);
         JqCommon.openPostWindow(url,datas);
-    },
-    openPostFalse:function(obj){
-        global_Object.value = $("#datevalue").val();
-        var url =contextPath+"/paas/clientdetailedhistory";
-        var datas={"transactionTypeName":$(obj).parents("tr").data("transactiontypename"),"serverIpAddress":$(obj).parents("tr").data("serveripaddress")==undefined?"":$(obj).parents("tr").data("serveripaddress"),"serverAppName":global_Object.flname,"type":(global_Object.type==undefined?"":global_Object.type),"value":global_Object.value,"clientAppName":"","clientIpAddress":"","status":"失败",historyPageType:"server"};
-        JqCommon.openPostWindow(url,datas);
     }
+
 }
